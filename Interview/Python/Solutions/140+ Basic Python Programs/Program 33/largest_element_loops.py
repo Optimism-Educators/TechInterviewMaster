0560a54f5ec3205ba6e5410168da508b560d2d99
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-21 14:27:08 +0530

    Update largest_element_loops.py

--- Interview/Python/Solutions/140+ Basic Python Programs/Program 33/largest_element_loops.py
+++ Interview/Python/Solutions/140+ Basic Python Programs/Program 33/largest_element_loops.py
@@ -23,4 +23,10 @@ if __name__ == "__main__":
     arr = input("Enter the elements of the array separated by spaces: ").split()
     arr = [int(i) for i in arr]  # Convert input strings to integers
     print(largest_element(arr))
-    
+# Output: 5
+# Input: [5, 4, 3, 2, 1]
+# Output: 5
+# Input: [1, 2, 3, 4, 8]
+# Output: 8
+# Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Output: 10
