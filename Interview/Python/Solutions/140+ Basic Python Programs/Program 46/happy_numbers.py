63a9340fafad4bbd7f83bab45f610b306fc3fe4a
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-04-23 15:07:23 +0530

    adding that for 1 to 100 full code

--- Interview/Python/Solutions/140+ Basic Python Programs/Program 46/happy_numbers.py
+++ Interview/Python/Solutions/140+ Basic Python Programs/Program 46/happy_numbers.py
@@ -2,5 +2,21 @@
 # Date: 28/05/2020
 # Discription:
 
-def happy_number():
+
+def is_happy_number(num):
+     seen = set()
+     while num != 1 and num not in seen:
+          seen.add(num)
+          num = sum(int(digit) ** 2 for digit in str(num))
+     return num == 1
+number = int(input("Enter a number to check if it's a happy number: "))
+if is_happy_number(number):
+     print(f"{number} is a Happy Number!")
+else:
+     print(f"{number} is not a Happy Number.")
      
+     
+print("Happy Numbers between 1 and 100:")
+for i in range(1, 101):
+     if is_happy_number(i):
+          print(i, end=" ")
